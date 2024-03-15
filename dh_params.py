5865535738bcba78cdbc1f7e19b2c0a90cce272d
Eric Rosen <eric.andrew.rosen@gmail.com>
2024-03-15 17:45:25 -0400

    get_dh_parameters

--- dh_params.py
+++ dh_params.py
@@ -2,9 +2,7 @@ from dataclasses import dataclass
 import numpy as np
 import numpy.typing as npt
 import math
-from typing import Union
 import matplotlib.pyplot as plt
-import itertools
 
 @dataclass
 class DHParameters:
@@ -38,16 +36,12 @@ def get_a_t_b(dh_parameter : DHParameters) -> npt.NDArray:
     mat_b[2,2] = math.cos(dh_parameter.theta)
     mat_b[1,2] = -math.sin(dh_parameter.theta)
     mat_b[2,1] = math.sin(dh_parameter.theta)
-
-    #print(f"mat_a: { mat_a}")
-    #print(f"mat_b: {mat_b}")
-
     return(np.matmul(mat_a,mat_b))
 
 def draw_frame_axes(world_t_frame : npt.NDArray, ax) -> None:
     world_p_origin = world_t_frame[1:,0]
     world_r_origin = world_t_frame[1:,1:]
-    # plot 
+    # plot axes
     ax.quiver(world_p_origin[0], world_p_origin[1], world_p_origin[2], world_r_origin[0,0], world_r_origin[1,0], world_r_origin[2,0], normalize=True, color="red")
     ax.quiver(world_p_origin[0], world_p_origin[1], world_p_origin[2], world_r_origin[0,1], world_r_origin[1,1], world_r_origin[2,1], normalize=True, color="green")
     ax.quiver(world_p_origin[0], world_p_origin[1], world_p_origin[2], world_r_origin[0,2], world_r_origin[1,2], world_r_origin[2,2], normalize=True, color="blue")
@@ -84,54 +78,60 @@ def visualize_dh_parameters(dh_parameters : list[DHParameters]) -> None:
     plt.show()
 
 # TODO: redo actual measurements
-    
-### Test script
-dh_01 = DHParameters(
-    d = 0.06985,
-    theta = np.pi, # TODO: user input
-    r = 0,
-    alpha = 0,
-)
+def get_dh_parameters(q_offset_1 : float = 0 ,
+                      q_offset_2 : float = 0 ,
+                      q_offset_3 : float = 0 ,
+                      q_offset_4 : float = 0 ,
+                      q_offset_5 : float = 0 ,
+                      q_offset_6 : float = 0 ,
+                      ):
+    ### Test script
+    dh_01 = DHParameters(
+        d = 0.06985,
+        theta = np.pi + q_offset_1, # TODO: user input
+        r = 0,
+        alpha = 0,
+    )
 
 
-dh_12 = DHParameters(
-    d = 0,#0.017462,
-    theta = np.pi/2.0, #0 TODO: user input
-    r = 0.01111,#-0.01111, # TODO: confirm this is okay to make negative and isn't a mistake?
-    alpha = np.pi/2.0,#np.pi/2.0
-)
+    dh_12 = DHParameters(
+        d = 0,#0.017462,
+        theta = np.pi/2.0 + q_offset_2, #0 TODO: user input
+        r = 0.01111,#-0.01111, # TODO: confirm this is okay to make negative and isn't a mistake?
+        alpha = np.pi/2.0,#np.pi/2.0
+    )
 
 
-dh_23 = DHParameters(
-    d = 0,
-    theta = 0, # TODO: user input
-    r = 0.08,
-    alpha = 0
-)
+    dh_23 = DHParameters(
+        d = 0,
+        theta = 0 + q_offset_3, # TODO: user input
+        r = 0.08,
+        alpha = 0
+    )
 
 
-dh_34 = DHParameters(
-    d = 0,
-    theta = np.pi/2.0, # TODO: user input
-    r = 0.08,
-    alpha = 0
-)
-dh_45 = DHParameters(
-    d = 0,
-    theta = 0, # TODO: user input
-    r = 0,
-    alpha = np.pi/2.0
-)
+    dh_34 = DHParameters(
+        d = 0,
+        theta = np.pi/2.0 + q_offset_4, # TODO: user input
+        r = 0.08,
+        alpha = 0
+    )
+    dh_45 = DHParameters(
+        d = 0,
+        theta = 0 + q_offset_5, # TODO: user input
+        r = 0,
+        alpha = np.pi/2.0
+    )
 
-dh_56 = DHParameters(
-    d = 0.09,# TODO: user input
-    theta = 0, 
-    r = 0,
-    alpha = 0
-)
+    dh_56 = DHParameters(
+        d = 0.09 + q_offset_6,# TODO: user input
+        theta = 0, 
+        r = 0,
+        alpha = 0
+    )
 
-learm_dh_parameters = [dh_01, dh_12,dh_23,dh_34,dh_45,dh_56]
+    learm_dh_parameters = [dh_01, dh_12,dh_23,dh_34,dh_45,dh_56]
+    return(learm_dh_parameters)
 
-print(learm_dh_parameters)
-
+learm_dh_parameters = get_dh_parameters(q_offset_3=np.pi/3.0)
 visualize_dh_parameters(learm_dh_parameters[:6])
