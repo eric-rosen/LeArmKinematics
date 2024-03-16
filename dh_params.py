1fc262d137239d423dc20df5ff138a5049f7a1dd
Eric Rosen <eric.andrew.rosen@gmail.com>
2024-03-16 11:00:55 -0400

    restructured visualization code

--- dh_params.py
+++ dh_params.py
@@ -7,6 +7,7 @@ import numpy as np
 import numpy.typing as npt
 import math
 import matplotlib.pyplot as plt
+from typing import List
 
 @dataclass
 class DHParameters:
@@ -62,10 +63,30 @@ def draw_link_axes(world_t_frame1 : npt.NDArray, world_t_frame2 : npt.NDArray, a
     # plot link
     ax.quiver(world_p_frame1[0], world_p_frame1[1], world_p_frame1[2], frame1_p_frame2[0], frame1_p_frame2[1], frame1_p_frame2[2], color="black")
 
-
-def visualize_dh_parameters(dh_parameters : list[DHParameters]) -> None:
+def get_link0_t_linki(dh_parameters : list[DHParameters]) -> List[npt.NDArray]:
     """
-    We assume the list of dh_parameters describe a serial kinematic chain
+    Given a list of DH parameters that start from 0 and go incrementally upward,
+    return all link0_t_linki.
+
+    For example, if you provide:
+    dh_parameters = [dh_01,dh_12,dh_23]
+    this will return
+    world_t_linki_list = [link0_t_link0,link0_t_link1,link0_t_link2,link0_t_link3]
+
+    Note that link0_t_link0 is always the 4x4 identity matrix (np.eye(4))
+    """
+    linki_t_linkj_list = [get_a_t_b(dh_parameter) for dh_parameter in  dh_parameters] # j = i+1
+    world_t_linki_list = [np.eye(4)]
+
+    for linki_t_linkj in linki_t_linkj_list:
+        world_t_linkj = np.matmul(world_t_linki_list[-1], linki_t_linkj)
+        world_t_linki_list.append(world_t_linkj)
+
+    return(world_t_linki_list)
+
+def init_matplotlib_fig():
+    """
+    3d figure initialization for LeArm
     """
     fig = plt.figure()
     ax = plt.axes(projection='3d')
@@ -77,20 +98,22 @@ def visualize_dh_parameters(dh_parameters : list[DHParameters]) -> None:
     ax.set_ylim((-0.15,.15))
     ax.set_zlim((-0.05,.25))
 
-    linki_t_linkj_list = [get_a_t_b(dh_parameter) for dh_parameter in  dh_parameters] # j = i+1
+    return (fig,ax)
 
-    print(linki_t_linkj_list)
 
-    world_t_linki_list = [np.eye(4)]
-    draw_frame_axes(world_t_linki_list[0], ax)
 
-    for linki_t_linkj in linki_t_linkj_list:
-        world_t_linkj = np.matmul(world_t_linki_list[-1], linki_t_linkj)
-        print(world_t_linkj)
-        draw_frame_axes(world_t_linkj, ax)
-        world_t_linki_list.append(world_t_linkj)
+def visualize_dh_parameters(world_t_linki_list : list[npt.NDArray],ax) -> None:
+    """
+    We assume the list of dh_parameters describe a serial kinematic chain
+    """
+    # Draw the frames
+    for link0_t_linkj in world_t_linki_list:
+        draw_frame_axes(link0_t_linkj, ax)
+    
+    # Draw the links
+    for linkj in range(len(world_t_linki_list)-1):
         # draw two most recent links
-        draw_link_axes(world_t_linki_list[-2], world_t_linki_list[-1], ax)
+        draw_link_axes(world_t_linki_list[linkj], world_t_linki_list[linkj+1], ax)
 
     print(len(world_t_linki_list))
 
@@ -152,5 +175,7 @@ def get_dh_parameters(q_offset_1 : float = 0 ,
     learm_dh_parameters = [dh_01, dh_12,dh_23,dh_34,dh_45,dh_56]
     return(learm_dh_parameters)
 
-learm_dh_parameters = get_dh_parameters()
-visualize_dh_parameters(learm_dh_parameters[:6])
+fig,ax = init_matplotlib_fig()
+learm_dh_parameters : list[DHParameters]= get_dh_parameters(q_offset_3=np.pi/4.0)
+world_t_linki_list : npt.NDArray = get_link0_t_linki(learm_dh_parameters)
+visualize_dh_parameters(world_t_linki_list,ax)
